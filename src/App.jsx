407653111301044e5f5f9751a38158452681f451
La1na <zolotukhinalp@gmail.com>
2025-01-25 10:39:02 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Rating from './rating';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 style={{ textAlign: 'center', marginTop: '20px' }}>Оцените наш сервис</h1>
+      <Rating />
+    </div>
+  );
+}
+
+export default App;
+
