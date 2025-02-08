72db782767b53751d6e96e5385cdea520b69fe18
La1na <zolotukhinalp@gmail.com>
2025-02-08 17:24:45 +0100

    Add files via upload

---
+++ src/components/SignIn.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function SignIn(){
+    return(
+        <div className="AllSignIn">
+            <h3 className="sign_in">Sign in</h3>
+            <h3 className="with">with</h3>
+        </div>
+    )
+}
+
+export default SignIn;
