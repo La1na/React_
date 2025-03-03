b4ba607ad7d343bdeb5a456677b73e552ed95d56
La1na <zolotukhinalp@gmail.com>
2025-03-03 09:37:20 +0100

    Add files via upload

---
+++ src/components/CatImage.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import styles from "./CatImage.module.css";
+
+const CatImage = () => {
+  const [imageUrl, setImageUrl] = useState("");
+
+  const fetchCatImage = async () => {
+    try {
+      const response = await axios.get("https://api.thecatapi.com/v1/images/search");
+      setImageUrl(response.data[0].url);
+    } catch (error) {
+      console.error("Ошибка при загрузке изображения", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchCatImage();
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Random Cat Image</h1>
+      {imageUrl && <img src={imageUrl} alt="Random Cat" className={styles.image} />}
+      <button className={styles.button} onClick={fetchCatImage}>
+        Load New Image
+      </button>
+    </div>
+  );
+};
+
+export default CatImage;
