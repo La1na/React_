fc409e628ca65a47823ae556d618c17d61a7eb3c
La1na <zolotukhinalp@gmail.com>
2025-03-15 09:26:42 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import UserList from './componets/UserList';
+
+const App = () => {
+  return (
+    <div>
+      <UserList />
+    </div>
+  );
+};
+
+export default App;
